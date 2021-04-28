32525ee284317c00a10aa168948e701349372bc9
Cyrine <amricyrine@gmail.com>
2021-04-28 13:03:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Form, Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 style={{color:"red",fontSize:"30px"}}>Login</h1>
+      <Form className="col-md-7">
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default App;
